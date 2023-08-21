8ea57342b74223d0917c6803d2fd1bea159f2e44
MasterKomand <142741566+MasterKomand@users.noreply.github.com>
2023-08-21 13:52:16 +0400

    Update HackFish.py

--- HackFish.py
+++ HackFish.py
@@ -1,64 +1,24 @@
-
 import os
-
-try:
-    from platform import platform
-except:
-    os.system("pip install platform")
-    from platform import platform
-
-
-
-puk = platform()[0], platform()[1], platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
-
-
-if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
-    delet = 'cls'
-    dr = '\\'
-else:
-    delet = 'clear'
-    dr = '/'
-
-
-
-os.system(delet)
-
-
-
-def on_startup():
-    print("<HackerRullerTools> Запуск!")
-on_startup()
-
-
-
-import tracemalloc
-
-from threading import Event
-
-try:
-    from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-    from aiogram import Bot, Dispatcher, executor, types
-    from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-    from aiogram.contrib.fsm_storage.memory import MemoryStorage
-except:
-    os.system("pip install aiogram")
-    from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-    from aiogram import Bot, Dispatcher, executor, types
-    from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-    from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-try:
-    import asyncio
-except:
-    os.system("pip install asyncio")
-    import asyncio
+import re
 
 try:
     import requests
 except:
     os.system("pip install requests")
     import requests
+
 import time
+
+import sys
+
+from os import system
+
+try:
+    from platform import platform
+except:
+    os.system("pip install platform")
+
+from time import sleep
 
 try:
     from art import *
@@ -68,6 +28,7 @@ except:
 
 
 def printtext(text):
+    os.system(delet)
     art1 = text2art(text)
     print(art1)
     time.sleep(1)
@@ -78,92 +39,70 @@ def printair(i):
         time.sleep(0.6)
         print()
 
-os.system(delet)
-printtext('HackFish')
-printtext('HackFish')
-printtext('HackFish')
-printtext('HackFish')
-printtext('HackFish')
-printtext('HackFish')
-time.sleep(4)
-os.system(delet)
-printtext('HackFish')
+
+puk = platform()[0], platform()[1], platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    delet = 'cls'
+    dr = '\\'
+else:
+    delet = 'clear'
+    dr = '/'
+
+printtext('H..')
+printtext('Ha..')
+printtext('Hac..')
+printtext('Hack..')
+printtext('Hacki..')
+printtext('Hackin..')
+printtext('Hacking..')
+printtext('HackingC..')
+printtext('HackingCa..')
+printtext('HackingCam')
+
+menu_ev = """1: Взлом камер Российской Федерациии!
+2: Об утилите!"""
+menu_number = 0
 
 
+def menu():
+    os.system(delet)
+    printtext('HackingCam')
+    print(menu_ev)
+    while True:
+        try:
+            menu_number = int(input('Выберите число: '))
+            if menu_number == 1:
+                print("\n")
+                os.system(delet)
+                try:
+                    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0'}
+                    for page in range(0, 82):
 
-tracemalloc.start()
+                        url = ("http://www.insecam.org/en/bycountry/RU/?page=" + str(page))
 
+                        res = requests.get(url, headers=headers)
+                        findip = re.findall('http://\d+.\d+.\d+.\d+:\d+', res.text)
+                        count = 0
 
+                        for _ in findip:
+                            hasil = findip[count]
+                            print("URL - камера: \033[1;37m", hasil)
+                            f = open('logs.txt', 'a')
+                            f.write(f'{findip}' + '\n')
+                            f.close()
 
-
-storage = MemoryStorage()
-
-bot = None
-token = None
-dp = None
-
-def registerbot():
-    try:
-        global bot
-        global token
-        global dp
-        token = str(input('Введите токен: '))
-        bot = Bot(token)
-        dp = Dispatcher(bot, storage=storage)
-    except:
-        print('Неккоректный токен!')
-        registerbot()
-
-
-registerbot()
-
-
-
-follower = types.InlineKeyboardMarkup(row_width=2)
-followerbutton1 = types.InlineKeyboardButton(text='Наш Канал 🖤', url='t.me/HackeeRullerToolsOfficial')
-followerbutton2 = types.InlineKeyboardButton(text='Наш чат 💤', url='https://t.me/HackerRullerTools')
-follower.add(followerbutton1).add(followerbutton2)
-
-markup = ReplyKeyboardMarkup()
-markup.add(
-    KeyboardButton(
-        text='✅ - Нажми',
-        request_contact=True
-    )
-)
-
-@dp.message_handler(commands='start')
-async def cmd_start(message: types.Message):
-    photo = 'https://www.promkod.ru/storage/editor/images/promokod-1.jpg'
-    await bot.send_photo(chat_id=message.chat.id, photo=photo, caption='<b>💥 - Привет, ты активировал Telegram бота! Он умеет накручивать пользователей на ваши каналы и группы! Нажми на кнопку и поделиться контактом!</b>', parse_mode='HTML', reply_markup=markup)
-    saveid = message.from_user.id
-    username = message.from_user.username
-    groupm = message.chat.id
+                            count += 1
+                except:
+                    print("Error")
+            if menu_number == 2:
+                print(
+                    'Утилита создано командой: HackerRullerTools\nТелеграмм создателей:\n@MasterKomand - создатель утилиты\n@TRAILEKS - помощник по коду в termux')
+        except:
+            print('Введите правильное число!')
+            time.sleep(3)
+            menu()
+            break
 
 
-
-
-
-@dp.message_handler(content_types=['contact'])
-async def clicker_test(message: types.Message):
-    mt = message.text
-    id = message.from_user.id
-    us = message.from_user.username
-    con = message.contact.phone_number
-    print(f'Bot > От {id} - @{us}: {mt} номер {con}')
-    await message.answer('Технический сбой! Подпишись на нас!', reply_markup=follower)
-
-
-
-
-
-
-async def end(_):
-    printtext('BotActive')
-
-
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=end)
-
+menu()  
